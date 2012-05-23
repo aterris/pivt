f7da4f8d0a2f58237f9637dc749e996ac61825b7
Andrew Terris <atterris@gmail.com>
2012-05-23 00:34:55 -0500

    removed unused file

--- lib/pivt/command.rb
+++
@@ -1,6 +0,0 @@
-module Pivt::Command
-  def self.list global_options, options, args
-    @project = Pivt::Project.new global_options[:u], global_options[:p], global_options[:project]
-    print_tasks()
-  end
-end
