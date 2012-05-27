4654407d040ff1270d8db47d8823030dad9adab9
Andrew Terris <atterris@gmail.com>
2012-05-27 17:49:31 -0500

    adds pending tests for task

--- spec/pivt/tasks_spec.rb
+++ spec/pivt/tasks_spec.rb
@@ -4,4 +4,24 @@ describe Pivt::Tasks do
   before(:each) do
   end
 
+  it 'can return all tasks'
+  it 'can return a single task'
+  it 'can create a new task'
+
+  it 'can be initialized'
+  it 'can be updated'
+  it 'can be deleted'
+  it 'can be printed'
+
+  it 'can be moved'
+  it 'can be popped'
+  it 'can be pushed'
+
+  it 'can be started'
+  it 'can be unstarted'
+  it 'can be finished'
+  it 'can be delivered'
+  it 'can be accepeted'
+  it 'can be rejectd'
+
 end
