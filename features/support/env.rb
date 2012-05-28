41722d685715a05b1108f497e40d58875f2dee39
Andrew Terris <atterris@gmail.com>
2012-05-28 14:35:41 -0500

    getting ready to try and get a working scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,4 @@
 require 'aruba/cucumber'
-require 'cucumber/rspec/doubles'
 require 'webmock/cucumber'
 require 'fileutils'
 
@@ -7,14 +6,7 @@ ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File:
 ENV['GLI_DEBUG'] = 'true'
 
 Before do
-  @real_home = ENV['HOME']
-  fake_home = File.join('/tmp','fake_home')
-  FileUtils.rm_rf fake_home, :secure => true
-  ENV['HOME'] = fake_home
 end
 
 After do
-  ENV['HOME'] = @real_home
-  config_file = File.join('/tmp','.pivt.rc.yaml')
-  FileUtils.rm config_file if File.exists? config_file
 end
