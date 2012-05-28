6ffba849da19a1757f2616428345d3a4de2788f7
Andrew Terris <atterris@gmail.com>
2012-05-27 20:16:01 -0500

    working on client specs

--- spec/lib/pivt/client_spec.rb
+++ spec/lib/pivt/client_spec.rb
@@ -19,6 +19,8 @@ describe Pivt::Client do
         :description => 'Another test task description',
         :current_state => 'unstarted'
         }]}.to_json)
+
+    Pivt::Client.config({:token => 'usertoken', :name => 'Andrew Terris', :project_id => '5'})
   end
 
   it 'can return authentication details' do
@@ -48,10 +50,56 @@ describe Pivt::Client do
       should raise_error('No Token')
   end
 
-  it 'can fetch a token'
-  it 'can determine if we have complete authentication info'
-  it 'can make a GET API request'
-  it 'can make a POST API request'
+  it 'can fetch a token' do
+    Pivt::Client.fetch_token('atterris@gmail.com', 'pwd')
+    WebMock.should have_requested(:get, "https://atterris%40gmail.com:pwd@www.pivotaltracker.com/services/v3/tokens/active").once
+  end
+
+  it 'can determine if we have complete authentication info' do
+    lambda {Pivt::Client.valid?}.
+      should_not raise_error
+
+    token = Pivt::Client.token
+    Pivt::Client.token = nil
+    lambda {Pivt::Client.valid?}.
+      should raise_error('No Token')
+    Pivt::Client.token = token
+
+    name = Pivt::Client.name
+    Pivt::Client.name = nil
+    lambda {Pivt::Client.valid?}.
+      should raise_error('No Name')
+    Pivt::Client.name = name
+
+    project_id = Pivt::Client.project_id
+    Pivt::Client.project_id = nil
+    lambda {Pivt::Client.valid?}.
+      should raise_error('No Project ID')
+    Pivt::Client.project_id = project_id
+  end
+
+  it 'can make a GET API request' do
+    query = {:filter => "mywork:#{Pivt::Client.name}"}
+    Pivt::Client.get("projects/5/stories", {:query => query})
+    WebMock.should have_requested(:get, "https://www.pivotaltracker.com/services/v3/projects/5/stories?filter=mywork:Andrew%20Terris").
+      with(:headers => {'X-TrackerToken' => 'usertoken'}).once
+  end
+
+  it 'can make a POST API request' do
+    body = {
+      :story => {
+        :name => 'Fake Name',
+        :description => 'Fake Description'
+      }
+    }.pivt_xml
+
+    response = Pivt::Client.post("projects/#{Pivt::Client.project_id}/stories", {:body => body})
+    WebMock.should have_requested(:post, "https://www.pivotaltracker.com/services/v3/projects/5/stories?filter=mywork:Andrew%20Terris").
+      with(:headers => {'X-TrackerToken' => 'usertoken'}, :body => 'boom').once
+
+
+  end
+
   it 'can make a PUT API request'
   it 'can make a DELETE API request'
   it 'can merge in authentication options'
